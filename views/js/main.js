e71c977a24206466d3c391ed0f224cb2056f5e7e
Luis Amez <Luis Amez>
2017-02-01 01:23:59 +0100

    Update main.js
    Decrease the amount of pizza when page loads

--- views/js/main.js
+++ views/js/main.js
@@ -542,7 +542,8 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  // Decrease the amount of pizza. Pizza is great, but 50 pizzas are enough!
+  for (var i = 0; i < 50; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
